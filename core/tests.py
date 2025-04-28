30636ac9084f1a0f0fe85e97fd24d678e0324b5a
erick.soi <ericksoi3709@gmail.com>
2025-04-28 16:12:01 +0300

    adding unit tests

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Customer, Order
 
-# Create your tests here.
+class CustomerOrderTestCase(TestCase):
+    def setUp(self):
+        self.customer = Customer.objects.create(name="Test Customer", code="TST001")
+    
+    def test_create_order(self):
+        order = Order.objects.create(customer=self.customer, item="Test Item", amount=10.50)
+        self.assertEqual(order.customer.name, "Test Customer")
+        self.assertEqual(order.item, "Test Item")
+        self.assertEqual(float(order.amount), 10.50)
+
+# Coverage check
+# Run tests with coverage:
+# coverage run manage.py test
+# coverage report -m
